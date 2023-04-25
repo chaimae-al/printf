32bdc82a9733393f21b87e031814cb87018c0da2
chaimae alaoui <chaimaehachimi01@gmail.com>
2023-04-25 15:43:32 -0700

    the committed files print_handler and utils.c

---
+++ print_handler.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * handle_print - a function that prints an argument based on its type
+ * @formt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
+ * @ind: index
+ * @buffer: Buffer array to the function
+ * @flags: get active flags
+ * @width: get width
+ * @precision:  specifies precision
+ * @size: specifies size
+ * Return: 1 or 2;
+ */
+
+int handle_print(const char *formt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	int i, x_len = 0, printed_chars = -1;
+	frmt formt_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percentage},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+		{'X', print_HEXA}, {'p', print_pointer}, {'S', print_un_printable},
+		{'r', print_reverse}, {'R', print_rot13str}, {'\0', NULL}
+	};
+	for (i = 0; formt_types[i].formt != '\0'; i++)
+		if (formt[*ind] == formt_types[i].formt)
+			return (formt_types[i].func(list, buffer, flags, width, precision, size));
+
+	if (formt_types[i].formt == '\0')
+	{
+		if (formt[*ind] == '\0')
+			return (-1);
+		x_len += write(1, "%%", 1);
+		if (formt[*ind - 1] == ' ')
+			x_len += write(1, " ", 1);
+		else if (width)
+		{
+			--(*ind);
+			while (formt[*ind] != ' ' && formt[*ind] != '%')
+				--(*ind);
+			if (formt[*ind] == ' ')
+				--(*ind);
+			return (1);
+		}
+		x_len += write(1, &formt[*ind], 1);
+		return (x_len);
+	}
+	return (printed_chars);
+}
---
+++ utils.c
@@ -0,0 +1,85 @@
+#include "main.h"
+
+/**
+ * is_printable - a function that testes  if a char is printable
+ * @c: Char to be tested
+ * Return: 1 if c is printable, 0 otherwise
+ */
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
+}
+
+/**
+ * hexa_code_append - a function that appends ascci
+ * in hexadecimal code to buffer
+ * @buffer: Array of chars.
+ * @i: index that scarts the appending
+ * @ascii_code: ASSCI CODE.
+ * Return: Always 3
+ */
+int hexa_code_append(char ascii_code, char buffer[], int i)
+{
+	char map_to[] = "0123456789ABCDEF";
+	/* The hexa format code is always 2 digits long */
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+
+	return (3);
+}
+
+/**
+ * is_digit - a function that  verifies if a char is a digit
+ * @c: Char to be tested
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+
+/**
+ * convert_size_number - a function that convers
+ * a number to the specified size
+ * @num: Number to be converted
+ * @size: Number indicating the type to be casted
+ * Return: the value of the casted number
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == LONG_S)
+		return (num);
+	else if (size == SHORT_S)
+		return ((short)num);
+
+	return ((int)num);
+}
+
+/**
+ * convert_size_unsigned -  a function that converts
+ * a number to the specified size
+ * @num: Number to be converted
+ * @size: Number indicating the type to be casted
+ * Return: the value of the  converted number
+ */
+long int convert_size_unsigned(unsigned long int num, int size)
+{
+	if (size == LONG_S)
+		return (num);
+	else if (size == SHORT_S)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
+}
